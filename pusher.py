aa5c942c2644cbd8c74bf7758a235557cd20b4ed
Crashed Code <s.agudelo1200@protonmail.com>
2020-05-27 05:38:57 +0000

    [Test. (./pusher.py)]

--- pusher.py
+++ pusher.py
@@ -8,16 +8,46 @@ import subprocess
 class Pusher:
     pass
 
-    def modified(self):
+    def __init__(self):
+        if os.path.isfile('.tasks'):
+            pass
+        else:
+            print('Pusher: you must first run the command:\ntasker  <project_link>')
+            exit(1)
+
+    def list_mod(self):
 
         try:
-            files = str(subprocess.check_output(
-                "git status -s . | grep ' M '", shell=True))
-            files = files.replace("b", '').replace(
+            """Modified files are captured"""
+            return str(subprocess.check_output(
+                "git status -s . | grep ' M '", shell=True)).replace("b", '').replace(
                 ' M ', '').replace("\\n", "\n").replace("'", '').strip().split()
-            print(files)
-        except Exception as ex:
-            print(ex)
+        except Exception:
+            print('Pusher: There are no modified files that need to be uploaded')
+            exit(1)
+
+    def list_new(self):
+
+        try:
+            """New files are captured"""
+            return str(subprocess.check_output(
+                "git status -s . | grep '?? '", shell=True)).replace("b", '').replace(
+                '?? ', '').replace("\\n", "\n").replace("'", '').strip().split()
+        except Exception:
+            print('Pusher: There are no new files that need to be uploaded')
+            exit(1)
+
+    def list_all(self):
+
+        try:
+            """All files are captured"""
+            return str(subprocess.check_output(
+                "git status -s .", shell=True)).replace("b", '').replace(
+                '?? ', '').replace(' M ', '').replace("\\n", "\n").replace("'", '').strip().split()
+
+        except Exception:
+            print('Pusher: There are no files that need to be uploaded')
+            exit(1)
 
     def force(self):
 
@@ -41,18 +71,54 @@ def get_args():
     args = sys.argv[1:]
     count = len(args)
 
-    if count is 2:
+    if count > 1:
+        print('Usage: pusher [OPTIONS]')
+        exit(1)
+
+    if count is 0:
+        return pusher.list_all()
+
+    if count is 1:
         method = args[0]
 
-        print(method)
-        if method is '-m':
-            pass
+        if method == '-m':
+            return pusher.list_mod()
+        elif method == '-n':
+            return pusher.list_new()
+        else:
+            print('Invalid option', method)
+            exit(1)
 
 
-test = Pusher()
-test.modified()
+if __name__ == "__main__":
 
+    pusher = Pusher()
+    files = get_args()
 
-""" if __name__ == "__main__":
+    tasksfile = open('.tasks', 'r')
+    tasks = tasksfile.read().splitlines()
 
-    get_args() """
+    changecommit = input(
+        'How should you handle commits?\n0 - Default\n1 - Edit\nOpt: ')
+    if changecommit not in ['0', '1']:
+        print('Pusher: Invalid option')
+        exit(1)
+    if changecommit is '0':
+        for itemname in files:
+            for task in tasks:
+                if task.find(itemname) >= 0:
+                    commit = task
+                else:
+                    commit = '{} added'.format(itemname)
+
+            os.system('git add {}'.format(itemname))
+            os.system("git commit -m '{}'".format(commit))
+        os.system('git push -u origin master')
+        print('\n  -> Files uploaded with their respective commit or filename per commit')
+    else:
+        for itemname in files:
+            commit = input('Insert commit: ')
+            os.system('git add {}'.format(itemname))
+            os.system("git commit -m '{}'".format(commit))
+        os.system('git push -u origin master')
+        print('\n  -> Files uploaded with custom commit')
